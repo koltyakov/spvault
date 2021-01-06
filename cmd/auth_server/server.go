5facd8240e849d2b189e264e6431c025744d32f6
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2021-01-06 19:05:34 +0300

    Thread safe registrations

--- cmd/auth_server/server.go
+++ cmd/auth_server/server.go
@@ -3,22 +3,22 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/google/uuid"
 	pb "github.com/koltyakov/spvault/proto"
 )
 
+// Server struct
 type Server struct {
 	pb.UnsafeVaultServer
-	regs map[string]*pb.AuthRequest // ToDo: add thread safety
+	regs sync.Map
 }
 
-func NewServer() *Server {
-	return &Server{
-		regs: map[string]*pb.AuthRequest{},
-	}
-}
+// NewServer Server constructor
+func NewServer() *Server { return &Server{} }
 
+// AuthenticateWithCreds authenticates using credentials
 func (s *Server) AuthenticateWithCreds(ctx context.Context, in *pb.AuthRequest) (*pb.AuthReply, error) {
 	authCnfg, err := resolveAuthCnfg(in)
 	if err != nil {
@@ -38,32 +38,34 @@ func (s *Server) AuthenticateWithCreds(ctx context.Context, in *pb.AuthRequest) 
 	return res, nil
 }
 
+// AuthenticateWithToken authenticates using registration token (previously uploaded credentials stored in memory)
 func (s *Server) AuthenticateWithToken(ctx context.Context, in *pb.TokenAuthRequest) (*pb.AuthReply, error) {
-	authRequest, ok := s.regs[in.RegToken]
+	reg, ok := s.regs.Load(in.RegToken)
 	if !ok {
 		return nil, fmt.Errorf("no registration found")
+	}
+	authRequest, ok := reg.(*pb.AuthRequest)
+	if !ok {
+		return nil, fmt.Errorf("can't cast auth registration")
 	}
 	return s.AuthenticateWithCreds(ctx, authRequest)
 }
 
+// Register registers authentication in server's memory
 func (s *Server) Register(ctx context.Context, in *pb.RegRequest) (*pb.RegReply, error) {
 	if in.RegToken == nil {
 		regToken := uuid.New().String()
 		in.RegToken = &regToken
 	}
-
-	s.regs[*in.RegToken] = in.AuthRequest
+	s.regs.Store(*in.RegToken, in.AuthRequest)
 	res := &pb.RegReply{
 		RegToken: *in.RegToken,
 	}
-
 	return res, nil
 }
 
+// DeRegister removes authentication registeration from server's memory
 func (s *Server) DeRegister(ctx context.Context, in *pb.DeRegRequest) (*pb.Empty, error) {
-	if _, ok := s.regs[in.RegToken]; ok {
-		delete(s.regs, in.RegToken)
-	}
-
+	s.regs.Delete(in.RegToken)
 	return &pb.Empty{}, nil
 }
